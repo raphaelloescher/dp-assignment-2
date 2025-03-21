c4cc704dee359e1ac506b712c8c114d44c3c81e8
Raphael <44301521+raphaelloescher@users.noreply.github.com>
2025-03-21 18:20:33 +0100

    Update processing.py

--- processing.py
+++ processing.py
@@ -2,7 +2,6 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 def clean_crimes():
-    # Read xml and extract inner rows (pd.read_xml, tries to read the outer tree element)
     with open("CrimesbyCountyandAgencySince1990.xml", "r") as f:
         xml_data = f.read()
 
@@ -15,19 +14,28 @@ def clean_crimes():
             row_data = {child.tag: child.text for child in inner_row}
             data_rows.append(row_data)
 
-    # Remove every entry for years 1990 - 2000
     df = pd.DataFrame(data_rows)
-    df = df[~df['year'].astype(int).between(1990, 2010)]
+    df = df.dropna()
+    df = df[df['months_reported'].astype(int) == 12]
+    df = df[~df['year'].astype(int).between(1990, 2000)]
+
+    numeric_columns = [
+        'total_index_crimes', 'violent', 'murder', 'forcible_rape', 'robbery',
+        'aggravated_assault', 'property', 'burglary', 'larceny', 'motor_vehicle_theft'
+    ]
+
+    for col in numeric_columns + ['year']:
+        df[col] = df[col].astype(int)
+
+    df = df.groupby(['county', 'year'], as_index=False)[numeric_columns].sum()
     df.to_xml("CrimesbyCountyandAgencySince2010.xml", index=False)
 
 def clean_unemployment():
     df = pd.read_csv('LocalAreaUnemploymentStatistics1976.csv')
-    print(df)
-    print(df.columns)
-    df = df[~df['Year'].astype(int).between(1976, 2010)]
-    print(df)
+    df = df[~df['Year'].astype(int).between(1976, 2000)]
+    df = df.drop(columns=['Month'])
+    df = df.groupby(['Area', 'Year'], as_index=False).mean(numeric_only=True)
     df.to_csv("LocalAreaUnemploymentStatistics2010.csv", index=False)
-    
     
 if __name__ == '__main__':
     clean_crimes()
